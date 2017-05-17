b1f8c7bc4693a5667324caeb0a2df7af1600bf0a
Lonk <rinke2005@gmail.com>
2017-05-17 16:36:36 +0200

    feat: header

--- src/Home.vue
+++ src/Home.vue
@@ -1,20 +1,62 @@
 <template>
     <div class="b-display">
+        <div class="b-header">
+            <div class="b-header__clock">{{ currentHour | hour }}</div>
+            <div class="b-header__message">Vos tweets sur le<br /><strong>#GalaUTT2017</strong></div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
+import './lib/dates.js';
 
+export default {
+    data () {
+        return {
+            currentHour: new Date()
+        };
+    },
+
+    mounted () {
+        setInterval(() => {
+          this.currentHour = new Date();
+        }, 1000);
+    }
 }
 </script>
 
 <style lang="css">
 .b-display {
-    background-image:url('./assets/background.png');
-    background-repeat:no-repeat;
-    background-size:100% 100%;
-    width:100%;
-    height:100%;
+    background-image: url('./assets/background.png');
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    width: 100%;
+    height: 100%;
+    font-family: Helvetica, Arial, sans-serif;
+    color: white;
+}
+
+.b-header {
+    width: 100%;
+    height: 10%;
+    color: #222222;
+    display: flex;
+    justify-content: space-between;
+}
+
+.b-header > .b-header__clock {
+    padding-top: 10px;
+    text-align: center;
+    width: 10%;
+    margin-left: 8%;
+    font-size: 6vmin;
+}
+
+.b-header > .b-header__message {
+    padding-top: 10px;
+    text-align: center;
+    width: 18%;
+    margin-right: 8%;
+    font-size: 3.5vmin;
 }
 </style>
